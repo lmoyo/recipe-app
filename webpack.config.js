8121c57c17141c5b2f3b4232c250ec4558ba3b15
Lo <lo@Los-MacBook-Pro-3.local>
2020-09-13 22:46:29 -0400

    config

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: ['babel-polyfill', './src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'), //need absolute path, use built in node package
+        filename: 'js/bundle.js'
+    },
+    node: {
+        fs: 'empty',
+        child_process: 'empty',
+        module: 'empty'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+}
